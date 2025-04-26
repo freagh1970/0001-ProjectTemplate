36c22eeb5af52c1a2682d490374df97d8306f45d
freagh1970 <df12553@gmail.com>
2025-04-26 09:28:32 -0400

    001 - First commit

--- FirstCommit.bat
+++ FirstCommit.bat
@@ -1,7 +1,8 @@
+REM First Commit
 git add *.*
-git commit -m "first commit"
+git commit -m "001 - First commit"
 git branch -M main
-git remote add origin https://github.com/freagh1970/0001-ProjectTemplate.git
+git remote add origin https://github.com/freagh1970/1000-CodeTesting.git
 git push -u origin main
 
 pause

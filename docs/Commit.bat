530fbdfa73e4435afaead3e5dafce9983a00a2a9
freagh1970 <df12553@gmail.com>
2025-04-27 12:52:03 -0400

    000-First commit

--- docs/Commit.bat
+++ docs/Commit.bat
@@ -3,9 +3,9 @@ CD C:\Data\Projects\02 - Control Engineering\02 - Rslogix 5000\02. Code Testing\
 
 REM Commit
 git add *.*
-git commit -m "000 - 20240427-00 - First commit"
+git commit -m "000-First commit"
 git branch -M main
-git remote add origin https://github.com/freagh1970/2000---CodingTesting.git
+git remote add origin https://github.com/freagh1970/1001_DesignPatterns.git
 git push -u origin main
 
 pause
